a45cfd259e94c64ec3fab158f979f0c8333c717f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 15:41:27 -0400

    fix(prompt): split p6_return_words word and variable into separate args (#1)
    * fix(prompt): split p6_return_words word and variable into separate args
    
    Refactor p6df-oneschema to use p6_return_words with separate word/variable args,
    add README.md, update deps to include p6common, and replace prompt::mod/profile::on/off
    with a simplified profile::mod.
    
    * fix(prompt): use double quotes for variable expansion in p6_return_words (SC2016)
    
    * chore(ci): add standard github workflows
    
    * fix(oneschema): restore single-quoted variable in profile::mod

--- init.zsh
+++ init.zsh
@@ -7,72 +7,21 @@
 #>
 ######################################################################
 p6df::modules::oneschema::deps() {
-  ModuleDeps=()
+  ModuleDeps=(p6m7g8-dotfiles/p6common)
 }
 
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::oneschema::prompt::mod()
+# Function: words oneschema $ONESCHEMA_API_KEY = p6df::modules::oneschema::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - oneschema $ONESCHEMA_API_KEY
 #
-#  Environment:	 P6_DFZ_PROFILE_ONESCHEMA
+#  Environment:	 ONESCHEMA_API_KEY
 #>
 ######################################################################
-p6df::modules::oneschema::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_ONESCHEMA"
+p6df::modules::oneschema::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    if p6_string_blank_NOT "$ONESCHEMA_API_KEY"; then
-      str="oneschema:\t  ${profile}: api"
-    fi
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::oneschema::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block
-#
-#  Environment:	 P6_DFZ_PROFILE_ONESCHEMA
-#>
-######################################################################
-p6df::modules::oneschema::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_ONESCHEMA" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::oneschema::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 P6_DFZ_PROFILE_ONESCHEMA
-#>
-######################################################################
-p6df::modules::oneschema::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_ONESCHEMA
-
-  p6_return_void
+  p6_return_words 'oneschema' '$ONESCHEMA_API_KEY'
 }
